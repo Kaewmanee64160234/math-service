19c5bb9e715a7f94818171d5fa839a4e384c6505
Kaewmanee <64160234@go.buu.ac.th>
2024-09-08 15:00:14 +0700

    feat: Add logger to MathController sum endpoint

--- src/math/math.controller.ts
+++ src/math/math.controller.ts
@@ -1,11 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { MathService } from './math.service';
 
 @Controller('math')
 export class MathController {
+  private logger = new Logger('MathController');
   constructor(private readonly mathService: MathService) {}
   @Post('sum')
   sum(@Body('numbers') numbers: number[]): number {
+    this.logger.log('Adding ' + numbers.toString());
     return this.mathService.accumulate(numbers);
   }
 }
