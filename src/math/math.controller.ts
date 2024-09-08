e0c91d85a2a719774c061cf09f73496cff614a0a
Kaewmanee <64160234@go.buu.ac.th>
2024-09-08 15:26:01 +0700

    refactor: Add @nestjs/microservices package and configure TCP microservice

--- src/math/math.controller.ts
+++ src/math/math.controller.ts
@@ -1,13 +1,21 @@
 import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { MathService } from './math.service';
+import { MessagePattern } from '@nestjs/microservices';
 
 @Controller('math')
 export class MathController {
   private logger = new Logger('MathController');
   constructor(private readonly mathService: MathService) {}
+
   @Post('sum')
   sum(@Body('numbers') numbers: number[]): number {
     this.logger.log('Adding ' + numbers.toString());
     return this.mathService.accumulate(numbers);
+  }
+
+  @MessagePattern({ cmd: 'sum' })
+  sum2(data: number[]): number {
+    this.logger.log('Adding ' + data.toString());
+    return this.mathService.accumulate(data);
   }
 }
